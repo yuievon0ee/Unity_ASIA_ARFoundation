1d8070ad6cf2687e6af77474a4b681da107b5604
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2020-11-24 11:58:00 +0800

    點擊判定

---
+++ Unity_ARFoundation/Assets/Scenes/ARManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARManager : MonoBehaviour
+{
+    [Header("點擊後要生成的物件")]
+    public GameObject obj;
+    [Header("AR管理器")]
+    public ARRaycastManager arManager;
+
+    private Vector2 pointMouse;
+
+    private List<ARRaycastHit> hits;
+
+    private void Tap()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointMouse = Input.mousePosition;
+            print(pointMouse);
+        }
+
+    }
+    private void Update()
+    {
+        Tap();
+    }
+
+}
